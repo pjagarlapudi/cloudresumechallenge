f0577ce1427eac3d64f398a6163a874b310131c1
Pratyusha <j.pratyusha@gmail.com>
2024-04-26 12:04:00 +0100

    Added markdown for numbered list

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,19 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "2vw";
+
+//get the visitor count from the api call[calling DB internally]
+//document.getElementById("visitor-count").value
+const apiUrl =
+  "https://i2aug9vc10.execute-api.eu-north-1.amazonaws.com/default/count";
+async function count(url = "", data = {}) {
+  const res = await fetch(apiUrl, {
+    method: "POST"
+    //referrerPolicy: "origin"
+    //cache: "no-cache"
+    //body: JSON.stringify(data)
+  });
+  return res.json();
+}
+count(apiUrl, {}).then((data) => {
+  //console.log("hello");
+  document.getElementById("visitor-count").value = data["count"];
+});
